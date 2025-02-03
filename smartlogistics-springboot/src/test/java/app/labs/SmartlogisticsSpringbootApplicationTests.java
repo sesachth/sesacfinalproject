4ed4bead95252a3c86d58adf7c2c4bb7be80516a
hantaeho <mashirogarden@gmail.com>
2025-02-03 15:42:16 +0900

    feat: 기본 Spring Boot 프로젝트 추가
    smartlogistics-fastapi - 기존 Python 소스코드들을 해당 폴더로 이동 및 FastAPI 활용할 예정
    smartlogistics-springboot - 기본 Spring Boot 프로젝트
    
    위 프로젝트에 추가된 Dependency: Spring Boot DevTools, Lombok, Spring Data JDBC,
    MyBatis Framework, MySQL Driver, Thymeleaf, Spring Security, Spring Web 등

---
+++ smartlogistics-springboot/src/test/java/app/labs/SmartlogisticsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package app.labs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartlogisticsSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
