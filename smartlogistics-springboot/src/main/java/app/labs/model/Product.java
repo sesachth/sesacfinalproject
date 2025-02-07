ff17579cac0e4999bdc602a94dc195680918ebe7
whacoscns <140468440+whacsocns@users.noreply.github.com>
2025-02-07 16:54:46 +0900

    Product.java 사라진 내용 복구

--- smartlogistics-springboot/src/main/java/app/labs/model/Product.java
+++ smartlogistics-springboot/src/main/java/app/labs/model/Product.java
@@ -1,9 +1,34 @@
 package app.labs.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class Product {
     private Long productId;
     private String name;
     private String category;
+
+    int productId;
+    String name;
+    float width;
+    float depth;
+    float height;
+    float weight;
+    boolean fragile;
+    String category;
+
+    public void setFragile(boolean fragile) {
+        this.fragile = fragile;
+    }
+
+    // 명확한 getter/setter 추가
+    public boolean isFragile() {
+        return fragile;
+    }
 
     // 기본 생성자
     public Product() {}
