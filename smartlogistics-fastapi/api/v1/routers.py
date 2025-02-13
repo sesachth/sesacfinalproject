74c7dc69ce8c2d817324feeb7f499461052cf622
hantaeho <mashirogarden@gmail.com>
2025-02-13 14:45:06 +0900

    feat: 관리자-적재 시뮬레이터 페이지 내 기능 추가
    FastAPI - 적재 최적화 결과 데이터를 JSON 형태로 보내는 엔드포인트를 추가하였습니다.
    Spring Boot - 관리자-적재 시뮬레이터 페이지를 구성하고, 파렛트 중 하나를 선택할 때마다 적재 결과를 출력하도록 구현하였습니다.

--- smartlogistics-fastapi/api/v1/routers.py
+++ smartlogistics-fastapi/api/v1/routers.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-from api.v1.endpoints import box
+from api.v1.endpoints import box, stacking_results
 
 api_router = APIRouter()
 api_router.include_router(box.router, tags=["boxes"])
+api_router.include_router(stacking_results.router, tags=["stacking_results"])
