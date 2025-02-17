f69afee41d63e401c8de15031f25a5abb660d31c
hantaeho <mashirogarden@gmail.com>
2025-02-17 20:35:24 +0900

    feat: 주문 상세 데이터에 대한 정렬 알고리즘 추가
    적재 시뮬레이터 페이지에서 사용될 적재 최적화 알고리즘을 처리하기 전, DB로부터 주문 상세 데이터를 불러와 이를 정렬하는 알고리즘을 추가하였습니다.

--- smartlogistics-fastapi/api/v1/routers.py
+++ smartlogistics-fastapi/api/v1/routers.py
@@ -3,4 +3,4 @@ from api.v1.endpoints import box, stacking_results
 
 api_router = APIRouter()
 api_router.include_router(box.router, tags=["boxes"])
-api_router.include_router(stacking_results.router, tags=["stacking_results"])
+api_router.include_router(stacking_results.router, tags=["stacking_results", "order_with_details"])
