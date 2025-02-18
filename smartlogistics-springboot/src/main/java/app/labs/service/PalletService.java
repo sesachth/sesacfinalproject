a562c0306eed405cea42abf400f615287f517745
whacoscns <140468440+whacsocns@users.noreply.github.com>
2025-02-18 22:02:24 +0900

    팔레트 페이지 필터링 완료, 물품 페이지 완료

--- smartlogistics-springboot/src/main/java/app/labs/service/PalletService.java
+++ smartlogistics-springboot/src/main/java/app/labs/service/PalletService.java
@@ -14,13 +14,35 @@ public class PalletService {
         this.palletRepository = palletRepository;
     }
 
-    public List<Pallet> getFilteredPalletList(int offset, int pageSize, String destination, String vehicleNumber) {
-        return palletRepository.getFilteredPalletList(offset, pageSize, destination, vehicleNumber);
+    /**
+     * 필터링된 팔렛트 리스트를 페이징하여 조회
+     */
+    public List<Pallet> getFilteredPalletList(
+            int offset, 
+            int pageSize, 
+            String palletId, 
+            String destination, 
+            String vehicleNumber) {
+        return palletRepository.getFilteredPalletList(
+            offset, 
+            pageSize, 
+            palletId, 
+            destination, 
+            vehicleNumber
+        );
     }
 
-    public int getTotalFilteredRecords( String destination, String vehicleNumber) {
-        return palletRepository.countTotalFilteredPallet( destination, vehicleNumber);
+    /**
+     * 필터링된 전체 팔렛트 수 조회
+     */
+    public int getTotalFilteredRecords(
+            String palletId, 
+            String destination, 
+            String vehicleNumber) {
+        return palletRepository.countTotalFilteredPallet(
+            palletId, 
+            destination, 
+            vehicleNumber
+        );
     }
-
-	
 }
