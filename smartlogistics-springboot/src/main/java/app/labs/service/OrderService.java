010557739b279c8f76a872de32940cf417ce336c
hantaeho <mashirogarden@gmail.com>
2025-02-21 17:56:32 +0900

    불필요한 import 문 삭제

--- smartlogistics-springboot/src/main/java/app/labs/service/OrderService.java
+++ smartlogistics-springboot/src/main/java/app/labs/service/OrderService.java
@@ -13,7 +13,6 @@ import java.util.Map;
 import java.util.Random;
 
 import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
